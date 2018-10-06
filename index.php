f0df5d3ec297e192570af63006eb8ee1342b5ace
Kenedy Inyam Fombang <ongabri@gmail.com>
2018-10-06 13:23:15 +0100

    Reviewproject modified

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Review project2</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" media="screen" href="css/bootstrap.css">
     <link rel="stylesheet" type="text/css" media="screen" href="css/style.css">
 </head>
@@ -14,22 +15,21 @@
             <div class="col-md-2"></div>
             <div class="col-md-8 style_div">
                 <form action="">
-                    <div class="col-md-5 form-group">
-                        <input type="text" class="form-control" placeholder="Enter Name">
+                    <div class="col-md-5 form-group style_form">
+                        <input type="text" class="form-control" placeholder="Enter Name...">
                     </div>
-                    <div class="col-md-5 form-group">
-                        <input type="email" class="form-control" placeholder="Enter Email">
+                    <div class="col-md-5 form-group style_form ">
+                        <input type="email" class="form-control" placeholder="Enter Email...">
                     </div>
-                    <div class="col-md-2 form-group">
-                        <input type="button" class="form-control btn btn-primary" value="Send">
+                    <div class="col-md-2 form-group style_form">
+                        <button type="button" class="form-control btn btn-primary"><i class="fas fa-paper-plane"></i> Send</button>
                     </div>    
                 </form>
             </div>
             <div class="col-md-2"></div>  
         </div>
     </div>
-
-    <script src="jquery.jquery-3.3.1.min.js"></script>
+    <script src="js/jquery.jquery-3.3.1.min.js"></script>
     <script src="js/bootstrap.js"></script>
     <script src="js/main.js"></script>
 </body>
