58188824f21f82f283fa2102a41b3489a14f08c7
Kenedy Inyam Fombang <ongabri@gmail.com>
2018-10-06 01:43:34 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Review project2</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/style.css">
+</head>
+<body>
+    <div class="container"> 
+        <div class="row">
+            <div class="col-md-2"></div>
+            <div class="col-md-8 style_div">
+                <form action="">
+                    <div class="col-md-5 form-group">
+                        <input type="text" class="form-control" placeholder="Enter Name">
+                    </div>
+                    <div class="col-md-5 form-group">
+                        <input type="email" class="form-control" placeholder="Enter Email">
+                    </div>
+                    <div class="col-md-2 form-group">
+                        <input type="button" class="form-control btn btn-primary" value="Send">
+                    </div>    
+                </form>
+            </div>
+            <div class="col-md-2"></div>  
+        </div>
+    </div>
+
+    <script src="jquery.jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>
